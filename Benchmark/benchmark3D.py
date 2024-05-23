324def34141122081e8868e7a5b54df500ae16ef
Remi_godzilla <remi.ducottet@telecom-paris.fr>
2024-05-23 09:37:19 +0700

    modifs du benchmark

--- Benchmark/benchmark3D.py
+++ Benchmark/benchmark3D.py
@@ -1,13 +1,18 @@
+#from trimesh import viewer
 import trimesh
-from vedo import load
+#from trimesh.viewer import windowed
+import pyautogui
+from vedo import load, Mesh
+import vedo
 import tkinter as tk
 from tkinter import ttk
 from tkinter import Tk, filedialog
-from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
-import os
-import glob
+#from trimesh.viewer import TrimeshScene
+from os import path, getcwd
+from glob import glob
 import csv
+#from vedo import deleteAll  # Import deleteAll function
 
 class ImageRater:
   def __init__(self):
@@ -15,9 +20,13 @@ class ImageRater:
         self.meshes = []
         self.prompts = []
         self.current_index = 0
-        self.photo_image = None
         self.init_window()
         self.preference_var=None
+        self.screen_width, self.screen_height = pyautogui.size()
+        self.screen_width*=0.625
+        self.screen_height*=0.625
+        self.quarter_width = int(self.screen_width * 0.25)
+        self.quarter_height = int(self.screen_height * 0.25)
   
   def get_out_dir(self):
       """
@@ -28,8 +37,6 @@ class ImageRater:
           self.window.destroy()
           self.get_meshes_and_prompts()
 
-
-
   def init_window(self):
       """
       initialisation of the Tkinter object
@@ -38,16 +45,12 @@ class ImageRater:
       self.window.title("Image to 3D model evaluation")
       out_dir_label = tk.Label(self.window, text="prompts and 3D assets repository")
       out_dir_label.pack()
-
       select_dir_button = tk.Button(self.window, text='Select your repository', command = self.get_out_dir)
       select_dir_button.pack()
 
-
   def load_meshes(self, filename):
     mesh = trimesh.load(filename+'/model1.obj')
-    #mesh.show()
     mesh2 = load(filename+'/model2.obj').texture(filename+'/model2.png')
-    #mesh2.show()  
     return mesh, mesh2
 
 
@@ -57,20 +60,18 @@ class ImageRater:
       """
       self.prompts = []
       self.meshes = []
-
       # Open the prompts file
-      with open(os.path.join(self.out_dir, 'prompts.txt'), 'r') as f:
-          self.prompts = [line.strip()[:-2] for line in f.readlines()]
+      with open(path.join(self.out_dir, 'prompts.txt'), 'r') as f:
+          self.prompts = [line.strip()[:] for line in f.readlines()]
 
       # Loop through subfolders and load meshes
-      for folder in sorted(glob.glob(os.path.join(self.out_dir, '[0-9]*/'))):  # Sort folders numerically
-          cwd = os.getcwd()
-          folder_name = os.path.relpath(folder, cwd)
+      for folder in sorted(glob(path.join(self.out_dir, '[0-9]*/'))):  # Sort folders numerically
+          cwd = getcwd()
+          folder_name = path.relpath(folder, cwd)
           #this is for evaluation of two models, triposr and CRM
-          if not os.path.isfile(os.path.join(folder, 'model1.obj')) or not os.path.isfile(os.path.join(folder, 'model2.obj')) or not os.path.isfile(os.path.join(folder, 'model2.png')):
+          if not path.isfile(path.join(folder, 'model1.obj')) or not path.isfile(path.join(folder, 'model2.obj')) or not path.isfile(path.join(folder, 'model2.png')):
               print(f"Warning: The folder {folder_name} - Isn't complete, it needs to have 2 .obj files and one .png file, please fix it this folder is skipped")
               continue
-
           # Load meshes using your existing function
           trimesh_mesh, vedo_mesh = self.load_meshes(folder_name)
           self.meshes.append((trimesh_mesh, vedo_mesh))
@@ -78,7 +79,6 @@ class ImageRater:
       # Validate number of meshes and prompts
       if len(self.meshes) != len(self.prompts):
           raise ValueError("Error: Number of folders must match the number of prompts.")
-
       self.create_rating_window()
       self.show_image_and_prompt()
 
@@ -87,21 +87,14 @@ class ImageRater:
     Saves the preference (Mesh 1 or Mesh 2) for the current prompt and displays the next one (if available).
     """
     selected_preference = self.preference_var.get()
-
-    # Validate selection
-
-
     if selected_preference == "Select Preference":
         print("Please select a preference (Mesh 1 or Mesh 2) before saving.")
         return
     
-
     self.current_index += 1
-
     with open(self.out_dir+"/scores.csv", "a", newline="") as csvfile:
       writer = csv.writer(csvfile)
-      writer.writerow([self.current_index, selected_preference, self.prompts[self.current_index]])
-
+      writer.writerow([self.current_index, selected_preference, self.prompts[self.current_index-1]])
 
     if self.current_index < len(self.meshes):
         self.show_image_and_prompt()
@@ -114,14 +107,11 @@ class ImageRater:
   
   def show_completion_message(self):
     """
-    Displays a message indicating that all images have been rated.
+    Displays a message indicating that all 3D assets have been rated.
     """
-    message = "All images have been rated!"
+    message = "All 3D assets have been rated!"
     completion_label = tk.Label(self.rating_window, text=message)
     completion_label.pack()
-
-
-
 
   def quit_app(self):
     """
@@ -136,24 +126,11 @@ class ImageRater:
     """
     self.rating_window = Tk()
     self.rating_window.title("Rate Meshes")
-
-    # Get window width and height
-    window_width = self.rating_window.winfo_screenwidth()
-    window_height = self.rating_window.winfo_screenheight()
-
     self.prompt_label = tk.Label(self.rating_window)
     self.prompt_label.pack()
-
-    # Image label
-    self.image_label = tk.Label(self.rating_window)
-    self.image_label.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
-
-
-
-    # Preference selection (combobox)
     self.preference_var = tk.StringVar(self.rating_window)
     self.preference_var.set("Select Preference")  # Initial value
-    preference_options = [("Mesh 1",1), ("Mesh 2",2)]
+    preference_options = [("Mesh 1"), ("Mesh 2")]
     preference_combobox = ttk.Combobox(self.rating_window, textvariable=self.preference_var, values=preference_options)
     preference_combobox.pack()
 
@@ -165,20 +142,41 @@ class ImageRater:
     self.quit_button = tk.Button(self.rating_window, text="Quit", command=self.quit_app)
     self.quit_button.pack()
 
+  def show_trimesh_mesh(self, mesh):
+      scene = trimesh.Scene([mesh])
+      from trimesh.viewer import windowed
+      viewer = windowed.SceneViewer(scene, resolution =(self.quarter_width*3, self.quarter_height*3))
+      viewer.set_location(1000, 1000)
+
+  def show_vedo_mesh(self, mesh):
+      """
+      Creates a Tkinter window to display the vedo mesh using vedo.show().
+      """
+      pos = (0, int(self.screen_height - self.quarter_height))  # Top-left corner at (0, remaining height)
+      size = (self.quarter_width*3, self.quarter_height*3)
+      if self.current_index>0 :
+         mesh.show(pos = pos, size=size, interactive = False).close()
+      vedo_window = mesh.show(pos = pos, size=size, interactive = False)  # Link vedo window to Tkinter window
+      vedo_window.close_on_empty_queue = True  # Close vedo window when Tkinter closes
+
+
   def show_image_and_prompt(self):
-    """
-    Displays the prompts and both meshes using mayavi.
-    """
-    trimesh_mesh1, trimesh_mesh2 = self.meshes[self.current_index]
+      """
+      Displays the prompts and opens separate windows for each mesh.
+      """
+      self.prompt_label.config(text=self.prompts[self.current_index])
+      trimesh_mesh1, vedo_mesh2 = self.meshes[self.current_index]
+      self.show_vedo_mesh(vedo_mesh2)
+      #self.show_trimesh_mesh(trimesh_mesh1)
+      window = pyglet.window.Window(width=self.quarter_width * 3, height=self.quarter_height * 3)
+      scene = trimesh.Scene([trimesh_mesh1])
+      @window.event
+      def on_draw():
+          window.clear()
+          scene.render()
 
-    # Update the prompt label
-    self.prompt_label.config(text=self.prompts[self.current_index])
-    
-    trimesh_mesh1.show()
-    trimesh_mesh2.show().close()
-
+      window.run()
 
 if __name__ == "__main__":
     rater = ImageRater()
-    #rater.init_window()
     tk.mainloop()
